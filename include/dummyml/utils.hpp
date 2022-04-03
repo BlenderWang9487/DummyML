8e1eb9a86640f8bcdff7da4e7d7f0b81e632f9b3
BlenderWang9487 <developinblend@gmail.com>
2022-04-04 01:19:50 +0800

    add softmax & norm distribution

--- include/dummyml/utils.hpp
+++ include/dummyml/utils.hpp
@@ -1,7 +1,39 @@
 #pragma once
-
+#include <vector>
+#include <cmath>
 namespace dummyml{
 
+class mean_variance
+{
+private:
+    std::vector<double> _mean;
+    std::vector<double> _variance;
+public:
+    mean_variance(){}
+    mean_variance(size_t size): _mean(size), _variance(size){}
+    inline size_t size() const {
+        return _mean.size();
+    }
+    inline const    double& mean    (size_t index) const {
+        return _mean.at(index);
+    }
+    inline          double& mean    (size_t index) {
+        return _mean.at(index);
+    }
+    inline const    double& variance(size_t index) const {
+        return _variance.at(index);
+    }
+    inline          double& variance(size_t index) {
+        return _variance.at(index);
+    }
+    inline static double normalDistribution(double m, double v, double x, bool ignore_const = false) {
+        return exp(-pow(x-m,2.0) / (ignore_const ? v : 2.0*v)) / sqrt(ignore_const ? v : v*M_PI_2);
+    }
+    inline static double logNormalDistribution(double m, double v, double x, bool ignore_const = false) {
+        return -pow(x-m,2.0) / (ignore_const ? v : 2.0*v) - 0.5*log(ignore_const ? v : v*M_PI_2);
+    }
+};
 
+std::vector<double> softmax(const std::vector<double>&);
 
 }
--- src/dummyml/utils.cpp
+++ src/dummyml/utils.cpp
@@ -1,2 +1,12 @@
 #include <utils.hpp>
 
+std::vector<double> dummyml::softmax(const std::vector<double>& v){
+    std::vector<double> ret(v.size());
+    double sum = 0.0;
+    for(size_t i = 0;i < v.size();++i)
+        sum += ret[i] = exp(v[i]);
+    for(size_t i = 0;i < v.size();++i)
+        ret[i] /= sum;
+    return ret;
+}
+
