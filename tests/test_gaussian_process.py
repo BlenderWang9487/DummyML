ead006894280cece5f717b182356be535e1d501a
BlenderWang9487 <developinblend@gmail.com>
2022-05-29 21:46:10 +0000

    add save load test cases

--- tests/test_gaussian_process.py
+++ tests/test_gaussian_process.py
@@ -1,8 +1,10 @@
 import dummyml as dmy
 import numpy as np
+import pytest
+import os
 
 def test_init():
-    gp = dmy.gaussian_process(0.2, dmy.kernel.LinearKernel)
+    gp = dmy.gaussian_process(0.2)
     assert gp.run_kernel(4,4) == 16.0
 
 def test_fit():
@@ -21,3 +23,31 @@ def test_predict():
     gp = dmy.gaussian_process(x, y, 0.2, dmy.kernel.RadialBasisFunctionKernel)
     
     assert len(gp(x[0])) == 2
+
+def test_save_load():
+    x = np.random.randn(100,3)
+    y = np.random.randn(100)
+    
+    gp = dmy.gaussian_process(0.2, dmy.kernel.RadialBasisFunctionKernel)
+    gp.fit(x,y)
+
+    inputX = np.random.randn(3)
+    results = gp(inputX)
+
+    model_dir = "./model/"
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+    model_path = model_dir + "gp.dmy"
+
+    gp.save(model_path)
+
+    gp2 = dmy.gaussian_process()
+    gp2.load(model_path)
+
+    assert np.all(results == gp2(inputX))
+
+    gp3 = dmy.gaussian_process(model_path)
+    assert np.all(results == gp3(inputX))
+
+    with pytest.raises(Exception) as e:
+        gp.load('./whateverFileDoesntExist.dmy')
--- tests/test_k_means.py
+++ tests/test_k_means.py
@@ -1,6 +1,7 @@
 import dummyml
 import numpy as np
 import pytest
+import os
 
 def test_fit_predict():
     input_dim = 784
@@ -20,3 +21,34 @@ def test_fit_predict():
     
     assert kms(np.random.rand(input_dim)) < k
     assert after_fit_iner < initial_iner
+
+def test_save_load():
+    input_dim = 784
+    k = 10
+    dataset_size = 60
+    kms = dummyml.k_means(input_dim,k)
+
+    x = np.random.rand(dataset_size,input_dim)
+    y = np.random.randint(0,k,size=dataset_size, dtype=np.int32)
+
+    for i in range(10):
+        kms.fit(x,y)
+    
+    means_before_save = kms.means()
+
+    model_dir = "./model/"
+    if not os.path.exists(model_dir):
+        os.mkdir(model_dir)
+    model_path = model_dir + "kms.dmy"
+
+    kms.save(model_path)
+
+    kms2 = dummyml.k_means(input_dim,k)
+    kms2.load(model_path)
+
+    means_after_save = kms2.means()
+
+    assert np.all(means_after_save == means_before_save)
+
+    with pytest.raises(Exception) as e:
+        kms.load('./whateverFileDoesntExist.dmy')
